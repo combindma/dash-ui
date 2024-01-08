e524837bbf7fb24b1e2dbc9d6ad5d85631363365
Combind <webmaster@combind.ma>
2024-01-08 16:13:35 +0100

    wip

--- resources/js/dash-ui.js
+++ resources/js/dash-ui.js
@@ -1,8 +1,6 @@
-import './components/_1_anim-menu-btn.js';
 import './components/_1_modal-window.js';
 import './components/_1_popover.js';
 import './components/_1_smooth-scrolling.js';
 import './components/_1_swipe-content.js';
-import './components/_1_switch-icon.js';
 import './components/_1_tabs.js';
 import './components/_1_tooltip.js';
