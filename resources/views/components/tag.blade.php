74f5c15660c08c47cab7a61a9b0c28b01d2ddf1c
Combind <webmaster@combind.ma>
2024-01-10 23:16:13 +0100

    wip

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,6 @@
-<span {{ $attributes->class(['inline-flex items-center gap-x-1 min-h-2 bg-neutral-200 rounded-lg px-1.5 py-px']) }}>
-    <span class="text-sm max-w-xs truncate">{{ $name }}</span>
-    <a href="{{ $url }}" class="leading-none text-neutral-500 hover:text-neutral-700 hover:bg-neutral-300 rounded-sm"><x-gmdi-close class="w-3 h-3 text-current"/></a>
+<span {{ $attributes->class(['inline-flex items-center gap-x-1 min-h-2 bg-neutral-200 rounded-lg pl-1.5 py-px']) }}>
+    <span class="max-w-xs truncate text-sm leading-none">{{ $name }}</span>
+    <a href="{{ $url }}" class="inline-flex items-center justify-center h-5 w-5 rounded-lg leading-none text-neutral-500 hover:bg-neutral-300 hover:text-neutral-700">
+        <x-gmdi-close class="h-3 w-3 text-current"/>
+    </a>
 </span>
