22c8e518db5f80e10bb90f3325be944b1498b9d7
Combind <webmaster@combind.ma>
2024-01-10 18:56:49 +0100

    Added circle loader

---
+++ resources/views/components/circle-loader.blade.php
@@ -0,0 +1,6 @@
+<div class="relative inline-block circle-loader--v1" role="alert">
+    <p class="circle-loader__label">Content is loading...</p>
+    <div aria-hidden="true">
+        <div class="circle-loader__circle"></div>
+    </div>
+</div>
---
+++ src/Components/CircleLoader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Combindma\DashUi\Components;
+
+use Illuminate\View\Component;
+
+class CircleLoader extends Component
+{
+    public function render()
+    {
+        return view('dash-ui::components.circle-loader');
+    }
+}
