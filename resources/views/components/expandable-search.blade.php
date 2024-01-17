2fff8d2ce102ffe086eefd892245d7f6bdb578f3
Combind <webmaster@combind.ma>
2024-01-18 00:17:51 +0100

    wip

--- resources/views/components/expandable-search.blade.php
+++ resources/views/components/expandable-search.blade.php
@@ -1,4 +1,4 @@
-<form {{ $attributes->class(['relative inline-block select-none js-expandable-search']) }}>
+<form class="relative inline-block select-none js-expandable-search">
     <label class="sr-only" for="expandable-search">Search</label>
     <input {{ $attributes->class(['expandable-search__input appearance-none box-border border-0 pl-2 lg:pl-3 text-sm text-transparent border-0 overflow-hidden rounded-lg hover:cursor-pointer focus:ring-2 focus:ring-primary-700 js-expandable-search__input']) }}>
 
