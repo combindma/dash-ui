e3ac347d6c8186c8f379c3d01af3d52620b67436
Combind <webmaster@combind.ma>
2024-01-18 00:16:41 +0100

    wip

--- resources/views/components/expandable-search.blade.php
+++ resources/views/components/expandable-search.blade.php
@@ -1,8 +1,8 @@
-<div class="relative inline-block select-none js-expandable-search">
+<form {{ $attributes->class(['relative inline-block select-none js-expandable-search']) }}>
     <label class="sr-only" for="expandable-search">Search</label>
     <input {{ $attributes->class(['expandable-search__input appearance-none box-border border-0 pl-2 lg:pl-3 text-sm text-transparent border-0 overflow-hidden rounded-lg hover:cursor-pointer focus:ring-2 focus:ring-primary-700 js-expandable-search__input']) }}>
 
-    <button class="expandable-search__btn">
+    <button type="submit" class="expandable-search__btn">
         <svg class="block m-auto h-3.5 w-3.5 text-inherit fill-current leading-none shrink-0" viewBox="0 0 24 24">
             <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3">
                 <circle cx ="10" cy="10" r="8" />
@@ -10,4 +10,4 @@
             </g>
         </svg>
     </button>
-</div>
+</form>
