84a75234a186da349cc6854f2b5f9b9624c07733
Combind <webmaster@combind.ma>
2024-01-19 12:14:38 +0100

    wip

--- resources/views/components/popover.blade.php
+++ resources/views/components/popover.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->class(['popover my-1 w-auto max-w-[200px] border-box fixed z-[5] js-popover js-tab-focus']) }}>
+<div {{ $attributes->class(['popover my-1 w-auto max-w-[fit-content] border-box fixed z-[5] js-popover js-tab-focus']) }}>
     <div class="card rounded-lg before:rounded-lg shadow overflow-auto bg-white">
         <div class="p-1.5 lg:p-2">
             <div class="flex flex-col gap-y-1">
