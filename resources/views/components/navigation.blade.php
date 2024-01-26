f90bffea6c5a88d14da2ebbc477988299e1c0a27
Combind <webmaster@combind.ma>
2024-01-26 11:01:11 +0100

    Fixed navigation issue

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,3 +1,24 @@
 <nav class="relative flex h-full flex-grow flex-col py-4 z-[1] lg:py-6">
     {{ $slot }}
 </nav>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const subNavItems = document.querySelectorAll('.sub-navigation li');
+        let foundActive = false;
+
+        subNavItems.forEach(function(li) {
+            if (li.querySelector('.sub-navigation--selected')) {
+                foundActive = true;
+                return;
+            }
+
+            if (!foundActive) {
+                const link = li.querySelector('a');
+                if (link) {
+                    link.classList.add('sub-navigation--before');
+                }
+            }
+        });
+    });
+</script>
