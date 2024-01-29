13e90f7e7691cf464c83b6e492ac2dda578b89c5
Combind <webmaster@combind.ma>
2024-01-29 18:30:46 +0100

    wip

--- resources/views/components/navigation-section.blade.php
+++ resources/views/components/navigation-section.blade.php
@@ -1,6 +1,6 @@
 <ul @class(['shrink-0 grow-0 basis-auto', 'pb-5' => !$sticky, 'sticky bottom-5 mt-auto' => $sticky])>
     @if($title)
-        <li class="relative flex flex-col px-4 mb-1">
+        <li class="relative flex flex-col px-2 mb-1">
             <span class="pl-3 text-neutral-500 text-sm font-semibold">{{ $title }}</span>
         </li>
     @endif
--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="relative flex h-full flex-grow flex-col py-4 z-[1] lg:py-6">
+<nav class="relative flex h-full flex-grow flex-col py-3 z-[1] lg:py-5">
     {{ $slot }}
 </nav>
 
